dd57e0a71729a6983b85d3f1e223b7fd2e62ffac
SHEEL PARIKH <91554427+sheel27@users.noreply.github.com>
2023-06-05 23:15:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,101 @@
+// let string = "";
+// let memory = 0;
+// let buttons = document.querySelectorAll(".button");
+
+// const evaluateExpression = (expression) => {
+//   return new Function("return" + expression)();
+// };
+
+// Array.from(buttons).forEach((button) => {
+//   button.addEventListener("click", (e) => {
+//     if (e.target.innerHTML == "=") {
+//       string = eval(string);
+//       document.querySelector("input").value = string;
+//     } else if (e.target.innerHTML == "AC") {
+//       string = "";
+//       document.querySelector("input").value = string;
+//     } else if (e.target.innerHTML === "M+") {
+//       memory += parseFloat(string);
+//       string = "";
+//       document.querySelector("input").value = memory;
+//     } else if (e.target.innerHTML === "M-") {
+//       memory -= parseFloat(string);
+//       string = "";
+//       document.querySelector("input").value = memory;
+//     } else if (e.target.innerHTML === "MC") {
+//       memory = 0;
+//     } else if (e.target.innerHTML === "√x") {
+//       string = BigInt(Math.sqrt(parseFloat(string)));
+//       document.querySelector("input").value = string;
+//     } else if (e.target.innerHTML === "x²") {
+//       string = BigInt(string) ** 2n;
+//       document.querySelector("input").value = string;
+//     } else if (e.target.innerHTML === "x!") {
+//       let num = parseInt(string);
+//       let result = 1n;
+//       for (let i = 1n; i <= BigInt(num); i++) {
+//         result *= i;
+//       }
+//       string = result.toString();
+//       document.querySelector("input").value = string;
+//     } else {
+//       console.log(e.target);
+//       string = string + e.target.innerHTML;
+//       document.querySelector("input").value = string;
+//     }
+//   });
+// });
+
+
+
+
 let string = "";
 let memory = 0;
 let buttons = document.querySelectorAll(".button");
 
 const evaluateExpression = (expression) => {
-  return new Function("return" + expression)();
+  return new Function("return " + expression.replace(/x/g, "*"))();
 };
 
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", (e) => {
-    if (e.target.innerHTML == "=") {
-      string = eval(string);
+    if (e.target.innerHTML === "=") {
+      if (string.includes("--")) {
+        string = string.replace("--", "+");
+      }
+      string = evaluateExpression(string);
       document.querySelector("input").value = string;
-    } else if (e.target.innerHTML == "AC") {
+    } else if (e.target.innerHTML === "AC") {
       string = "";
       document.querySelector("input").value = string;
     } else if (e.target.innerHTML === "M+") {
       memory += parseFloat(string);
-      string = "";
       document.querySelector("input").value = memory;
     } else if (e.target.innerHTML === "M-") {
       memory -= parseFloat(string);
-      string = "";
       document.querySelector("input").value = memory;
     } else if (e.target.innerHTML === "MC") {
       memory = 0;
     } else if (e.target.innerHTML === "√x") {
-      string = BigInt(Math.sqrt(parseFloat(string)));
+      string = Math.sqrt(parseFloat(string));
       document.querySelector("input").value = string;
     } else if (e.target.innerHTML === "x²") {
-      string = BigInt(string) ** 2n;
+      string = parseFloat(string) ** 2;
       document.querySelector("input").value = string;
     } else if (e.target.innerHTML === "x!") {
       let num = parseInt(string);
-      let result = 1n;
-      for (let i = 1n; i <= BigInt(num); i++) {
+      let result = 1;
+      for (let i = 1; i <= num; i++) {
         result *= i;
       }
       string = result.toString();
       document.querySelector("input").value = string;
+    } else if (e.target.innerHTML === "/") {
+      string += "/";
+      document.querySelector("input").value = string;
     } else {
       console.log(e.target);
-      string = string + e.target.innerHTML;
+      string += e.target.innerHTML;
       document.querySelector("input").value = string;
     }
   });
